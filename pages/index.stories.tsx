cfd72f301ab2fee7ee3ae5cce461c5b3fe487019
aaron <aron.moles@gmail.com>
2022-01-30 20:26:55 +0100

    feat: add storybook

---
+++ pages/index.stories.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import { ComponentMeta, ComponentStory } from '@storybook/react';
+import Home from './index';
+
+export default {
+    title: 'Pages/Home',
+    component: Home,
+} as ComponentMeta<typeof Home>;
+
+const Template: ComponentStory<typeof Home> = (args) => <Home {...args} />;
+
+export const Normal = Template.bind({});
+Normal.args = {
+};
+

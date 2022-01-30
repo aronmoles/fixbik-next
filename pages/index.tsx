573104b965b8333a3efb89ebf083896b2408917e
aaron <aron.moles@gmail.com>
2022-01-30 19:54:13 +0100

    feat: add image into Home

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,4 +1,5 @@
 import Link from 'next/link'
+import Image from 'next/image'
 import { useAuth } from '../contexts/shared/infrastructure/auth/AuthProvider';
 import { NextApplicationPage } from './next-application-page';
 import Route from './route';
@@ -10,8 +11,9 @@ const Home: NextApplicationPage = () => {
         <>
             <h1>FixBik</h1>
             <p className="a">User {isAuth ? 'logged' : 'not logged'}</p>
-            <Link href={Route.AUTHENTICATE}>Login</Link>
-            <a href={'#'} onClick={() => logout()}>Logout</a>
+            <p><Link href={Route.AUTHENTICATE}>Login</Link></p>
+            <p><a href={'#'} onClick={() => logout()}>Logout</a></p>
+            <Image src="/vercel.svg" alt="Vercel Logo" width={72} height={16} />
         </>
     )
 }
